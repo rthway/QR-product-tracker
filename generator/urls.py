134c1d50bc999fdad6c02cb1d105d48b1eeaf789
Ramina <thaparamina999@gmail.com>
2024-02-20 08:23:02 +0545

    qr code

--- generator/urls.py
+++ generator/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('generate/', views.generate_serial_numbers, name='generate_serial_numbers'),
-    path('',views.view_generated_serial_numbers,name='view_qr_code')
+    path('',views.view_generated_serial_numbers,name='view_qr_code'),
+    path('print_qr_code/<str:bundle_number>/', views.print_qr_code, name='print_qr_code'),
+    # Other URL patterns
 ]
+
--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,28 @@
 # generator/views.py
 from django.shortcuts import render
 from .models import SerialNumber
+import qrcode
+from django.http import HttpResponse
+from io import BytesIO
 
+def print_qr_code(request, bundle_number):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(bundle_number)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img_io = BytesIO()
+    img.save(img_io, format='PNG')
+    img_io.seek(0)
+
+    response = HttpResponse(img_io, content_type='image/png')
+    response['Content-Disposition'] = 'inline; filename="qrcode.png"'
+    return response
 def generate_serial_numbers(request):
     if request.method == 'POST':
         product_name = request.POST.get('product_name')
@@ -15,4 +36,6 @@ def generate_serial_numbers(request):
 
 def  view_generated_serial_numbers(request):
     serial_numbers = SerialNumber.objects.all()
+    for serial_number in serial_numbers:
+        serial_number.bundle_number = serial_number.serial_number[:19]
     return  render(request, 'qr_code_list.html', {'serial_numbers': serial_numbers})
